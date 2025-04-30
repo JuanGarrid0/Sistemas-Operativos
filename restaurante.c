71c950a5a34b35c8871b7abc84de020cc5a636a2
JuanGarrid0 <juangarrido@opendeusto.es>
2025-05-01 01:19:49 +0200

    Uso semaforos en cocina

--- restaurante.c
+++ restaurante.c
@@ -12,6 +12,7 @@
 #define  MQ_NAME "/mq_cola_queue"
 
 pid_t pid_sala, pid_cocina;
+sem_t sem_preparado, sem_cocinado, sem_emplatado;
 
 struct mq_attr attributes = {
     .mq_flags = 0,
@@ -22,6 +23,7 @@ struct mq_attr attributes = {
 
 
 
+
 int tiempo_aleatorio(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
@@ -29,24 +31,26 @@ int tiempo_aleatorio(int min, int max) {
 void* preparar_ingredientes(void* args) {
     char buffer[128];
     while (1) {
-
+        sem_wait(&sem_preparado);
         printf("[Preparación] Esperando comanda...\n");
 
         printf("[Preparación] Recibida comanda: %s. Preparando ingredientes...\n", buffer);
         sleep(tiempo_aleatorio(3, 6));
+       
         printf("[Preparación] Ingredientes listos.\n");
-
+        sem_post(&sem_cocinado);
     }
 }
 
 void* cocinar(void* arg) {
 
     while (1) {
-
+        sem_wait(&sem_cocinado);
         printf("[Cocina] Cocinando plato...\n");
         sleep(tiempo_aleatorio(4, 8));
+     
         printf("[Cocina] Plato cocinado.\n");
-
+        sem_post(&sem_emplatado);
     }
 }
 
@@ -54,17 +58,17 @@ void* cocinar(void* arg) {
 void* emplatar(void* arg) {
 
     while (1) {
-
+        sem_wait(&sem_emplatado);
         printf("[Emplatado] Emplatando el plato...\n");
         sleep(tiempo_aleatorio(2, 4));
+   
         printf("[Emplatado] Plato listo y emplatado.\n");
+        sem_post(&sem_preparado);
 
     }
 }
 
 int main(int argc, char* argv[]) {
-
-   
     mqd_t queue = mq_open(MQ_NAME , O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR, &attributes);
     Comanda pedido;
     strcpy(pedido.msg,"Una de champiñones");
@@ -72,35 +76,35 @@ int main(int argc, char* argv[]) {
 
 
     pid_sala = fork();
-    pthread_t t1, t2, t3;
 
     if (pid_sala != 0) {
         pid_cocina = fork();
         if (pid_cocina != 0) {
 		/* Proceso padre */
-
+      
 
 
         } else {
             /* Proceso Cocina */
+            pthread_t t1, t2, t3;
+            sem_init(&sem_preparado, 0, 1);
+            sem_init(&sem_cocinado, 0, 0);
+            sem_init(&sem_emplatado, 0, 0);
             printf("[Cocina] Comienzo de la preparación de platos...\n");
-
             if( pthread_create(&t1, NULL, &preparar_ingredientes, NULL)  != 0) {
                 return 1;
             };
-            pthread_join(t1, NULL);
+  
 
             if( pthread_create(&t2, NULL, &cocinar, NULL) != 0) {
                 return 2;
             };
-            pthread_join(t2,NULL);
+
 
             if( pthread_create(&t3, NULL, &emplatar, NULL) != 0) {
                 return 3;
-            }
-            pthread_join(t2,NULL);
-
-
+            };
+          
 
         }
     } else {
--- restaurante.h
+++ restaurante.h
@@ -1,10 +1,6 @@
 #ifndef RESTAURANTE_H_
 #define RESTAURANTE_H_
 
-
- 
-
-
 typedef struct{
     char msg[200];
 } Comanda;
